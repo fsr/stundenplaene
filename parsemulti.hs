159b95fdb0cad758c3201a1603d6a69ff60be5ab
Sebastian J. Mielke <sjm@sjmielke.com>
2014-09-16 12:06:38 +0200

    I thought the HTML we received this year would be way easier to parse. Hah. So parsing doesn't really work, but at least printing and data structures have been updated :)

--- parsemulti.hs
+++ parsemulti.hs
@@ -1,79 +1,144 @@
-import Data.List (intercalate, transpose)
-import Data.List.Split (splitOn)
-replace old new = intercalate new . splitOn old
+import Text.XML.Light
+import Data.List (transpose)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Maybe (fromJust, catMaybes)
+import Debug.Trace (trace)
 
-data Slot = Element {sname::String, sweekday::String, sstart::String, send::String, sroom::String, steacher::String} | Nil deriving Eq
+data Event = Event { sname    :: String
+                   , sweekday :: String
+                   , sstart   :: String
+                   , send     :: String
+                   , sroom    :: String
+                   , steacher :: String
+                   }
+             deriving Eq
+type Slot = Maybe Event
 type Day = [Slot]
-type Time = [Slot]
-type Plan = [Day]
+type Timeslot = [Slot]
+-- TODO [Slot] ambigous -> newtype
 
 germanweekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
-starttimes = ["7:30", "9:20", "11:10", "13:00", "14:50", "16:40", "18:30"]
-endtimes = ["9:00", "10:50", "12:40", "14:30", "16:20", "18:10", "20:00"]
+starttimes = ["7:30", "9:20",  "11:10", "13:00", "14:50", "16:40", "18:30"]
+endtimes   = ["9:00", "10:50", "12:40", "14:30", "16:20", "18:10", "20:00"]
+timespans = zipWith (\a b -> a++" - "++b) starttimes endtimes
 
-main = do
-	readFile "studsets_ws1314.txt" >>= writeFile "clean.txt" . cleanup
-	readFile "clean.txt" >>= writeFile "index.htm" . concat . map (getPlanAsTable . parseInputText) . tail . splitOn "INF 1"
+main = --readFile "/home/sjm/programming/stundenplaene/input.htm" >>= writeFile "output.htm" . concatMap ppContent . concatMap getPlanAsTable . (:[]) . parseInputText
+       readFile "/home/sjm/programming/stundenplaene/input.htm" >>= writeFile "/tmp/output.htm" . concatMap ppContent . concatMap getPlanAsTable . parseInputHtml
 
-getPlanAsTable :: (String, Plan) -> String
-getPlanAsTable (pname,p) = "\n<h2>"++pname++"</h2>\n<table border='2'><tbody>\n" ++ weekdays
-								++ (printtimeslotlist . transpose $ p) ++ "</tbody></table><br /><br />\n"
-	where
-		weekdays = "\t<tr>\n\t\t<td class='plan_leer'>"++pname++"</td>\n" ++
-						(concat $ map (\s -> "\t\t<td colspan='2' class='plan_tage'>" ++ s ++ "</td>\n") germanweekdays) ++ "\t</tr>\n"
-		printtimeslotlist = concat . map printtimeslot . zip [1..]
-		printtimeslot (nr, ts) = "\t<tr>\n\t\t<td class='plan_stunden'>"++(show nr)++". DS</td>\n" ++ (concat . map printslotname $ ts)
-								++ "\t</tr>\n\t<tr>\n\t\t<td class='plan_uhrzeit'>"++(timefor nr)++"</td>\n"
-								++ (concat . map printslotdetails $ ts) ++ "\t\t</tr>\n"
-		printslotname Nil = "\t\t<td colspan='2' class='plan_name'></td>\n"
-		printslotname (Element name _ _ _ _ _) = "\t\t<td colspan='2' class='plan_name'>"++name++"</td>\n"
-		printslotdetails Nil = "\t\t<td class='plan_dozent'></td>\n\t\t<td class='plan_raum'></td>\n"
-		printslotdetails (Element _ _ _ _ room teacher) = "\t\t<td class='plan_dozent'>"++teacher++
-															"</td>\n\t\t<td class='plan_raum'>"++room++"</td>\n"
-		timefor i = nicetimespans!!(i-1)
-		nicetimespans = zipWith (\a b -> a++" - "++b) starttimes endtimes
+getPlanAsTable :: (String, [Day]) -> [Content]
+getPlanAsTable (pname, p) = map Elem [ unode "h2" pname
+                                     , add_attr (Attr (unqual "border") "2")
+                                                (unode "table" $ weekdaysRow
+                                                                 : ( concatMap getTimeslotRows
+                                                                   $ zip [1..]
+                                                                   $ transpose p ))
+                                                               -- transpose because tables are read horizontally
+                                     , unode "br" ()
+                                     , unode "br" ()
+                                     ]
+    where
+        weekdaysRow :: Element
+        weekdaysRow = unode "tr"
+                    $ giveClass "plan_leer" (unode "td" pname)
+                      : map ( add_attr (Attr (unqual "colspan") "2")
+                            . giveClass "plan_tage"
+                            . unode "td"
+                            )
+                          germanweekdays
+        getTimeslotRows :: (Int, Timeslot) -> [Element]
+        getTimeslotRows (nr, slots) = [ unode "tr"
+                                      $ giveClass "plan_stunden" (unode "td" $ show nr ++ ". DS")
+                                        : (map getSlotName slots)
+                                      , unode "tr"
+                                      $ giveClass "plan_uhrzeit" (unode "td" $ timespans !! (nr - 1))
+                                        : (concatMap getSlotDetails slots)
+                                      ]
+        getSlotName :: Slot -> Element
+        getSlotName slot = add_attr (Attr (unqual "colspan") "2")
+                         . giveClass "plan_name"
+                         . unode "td"
+                         $ case slot of
+                               Nothing -> ""
+                               Just e  -> sname e
+        getSlotDetails :: Slot -> [Element]
+        getSlotDetails slot = [ giveClass "plan_dozent"
+                              . unode "td"
+                              $ case slot of
+                                    Nothing -> ""
+                                    Just e  -> steacher e
+                              , giveClass "plan_raum"
+                              . unode "td"
+                              $ case slot of
+                                    Nothing -> ""
+                                    Just e  -> sroom e
+                              ]
+        giveClass :: String -> Element -> Element
+        giveClass str = add_attr (Attr (unqual "class") str)
 
-parseInputText :: String -> (String, Plan)
+parseInputHtml :: String -> [(String, [Day])]
+parseInputHtml = (:[]) . parseTable . head
+               . chunksOf 2 -- each plan contains two tables
+               . findElements (QName "table" (Just "http://www.w3.org/1999/xhtml") Nothing)
+               . fromJust
+               . parseXMLDoc
+    where
+        parseTable :: [Element] -> (String, [Day])
+        parseTable [headtable, contenttable] = ( getCaption
+                                               . head
+                                               . elContent
+                                               . firstChild
+                                               . lastChild
+                                               . lastChild
+                                               . lastChild
+                                               . lastChild
+                                               $ headtable, sorteventsintoplan
+                                                          . (\o -> trace (show $ length o) o)
+                                                          . catMaybes
+                                                          . concatMap (uncurry getEventsPerTimeslot)
+                                                          . zip [0..]
+                                                          . map ( zip [0..]
+                                                                . map head     -- drop every 
+                                                                . chunksOf 2   -- second (useless), after we
+                                                                . repeatEvents -- repeat proper 2-hour cells once
+                                                                . tail
+                                                                . elChildren)
+                                                          . tail
+                                                          . elChildren
+                                                          $ contenttable )
+            where
+                getCaption (Text (CData _ s _)) = drop 3 . last $ splitOn " " s
+                firstChild = head . elChildren
+                lastChild = last . elChildren
+                getEventsPerTimeslot :: Int -> [(Int, Element)] -> [Slot]
+                getEventsPerTimeslot slotnr = map (getSlot slotnr)
+                getSlot :: Int -> (Int, Element) -> Slot
+                getSlot slotnr (daynr, e) = if (max slotnr daynr > 6) then error (show . head . tail $ elContent e) else case tail $ elContent e of
+                                                 [] -> Nothing
+                                                 (Elem table : _)  -> Just $ Event "Foo"
+                                                                                   (germanweekdays !! daynr)
+                                                                                   (starttimes !! slotnr)
+                                                                                   (endtimes !! slotnr)
+                                                                                   "y"
+                                                                                   "x"
+                                                 _ -> Nothing
+                repeatEvents :: [Element] -> [Element]
+                repeatEvents [] = []
+                repeatEvents (x : xs) = if findAttr (QName "colspan" (Just "http://www.w3.org/1999/xhtml") Nothing) x == Nothing
+                                        then x : repeatEvents xs
+                                        else x : x : repeatEvents xs
+
+parseInputText :: String -> (String, [Day])
 parseInputText fullplan = (last . splitOn " " . head . lines $ fullplan, createplan . tail . lines $ fullplan)
-	where
-		createplan = sorteventsintoplan . map createeventfromstring
-		createeventfromstring = createeventfromlist . splitOn "\t"
-		createeventfromlist sl = if length sl < 7 then (error $ "Too short line: " ++ (show sl)) else
-									Element (sl!!1) (sl!!2) (sl!!3) (sl!!4) (sl!!5) (niceteacher $ sl!!6)
-		niceteacher = clearifunannounced . last . splitOn " "
-		clearifunannounced name =  if length name < 2 then error $ "Too short name: " ++ name else
-								if name!!0 == 'N' && name!!1 == '.' && name!!2 == 'N' && name!!3 == '.' then "" else
-								if name!!0 == 'Z' && name!!1 == 'Z' then "" else name
+    where
+        createplan = sorteventsintoplan . map createeventfromstring
+        createeventfromstring = createeventfromlist . splitOn "\t"
+        createeventfromlist sl = if length sl < 7 then (error $ "Too short line: " ++ (show sl)) else
+                                    Event (sl!!1) (sl!!2) (sl!!3) (sl!!4) (sl!!5) (niceteacher $ sl!!6)
+        niceteacher = clearifunannounced . last . splitOn " "
+        clearifunannounced name =  if length name < 2 then error $ "Too short name: " ++ name else
+                                if name!!0 == 'N' && name!!1 == '.' && name!!2 == 'N' && name!!3 == '.' then "" else
+                                if name!!0 == 'Z' && name!!1 == 'Z' then "" else name
 
-sorteventsintoplan :: [Slot] -> Plan
+sorteventsintoplan :: [Event] -> [Day]
 sorteventsintoplan ellist = map (\w -> createday . filter ((==) w . sweekday) $ ellist) germanweekdays
-	where createday els = map (\t -> if t `elem` (map sstart els) then (head . filter ((==) t . sstart) $ els) else Nil) starttimes
-
--- I really hope we get a nice machine-readable file next time so stuff like this won't be necessary. Sigh.
-
-cleanup :: String -> String
-cleanup =     replace "65 INF" "65\tINF"
-			. replace "40 INF" "40\tINF"
-			. replace "\tV\tAlgorithmen und Datenstrukturen " "\tAuD VL\t"
-			. replace "\tV\tAlgorithmen und Datenstrukturen\t" "\tAuD VL\t"
-			. replace " U\tMathematik 1 für Informatiker: Lineare Algebra " "\tLAG Ü\t"
-			. replace " U\tMathematik 1 für Informatiker: Diskrete Strukturen " "\tDIS Ü\t"
-			. replace " U\tMathematik 1 für Informatiker: Lineare Algebra\t" "\tLAG Ü\t"
-			. replace " U\tMathematik 1 für Informatiker: Diskrete Strukturen\t" "\tDIS Ü\t"
-			. replace " U\tEinführung in die Medieninformatik " "\tEMI Ü\t"
-			. replace " V\tEinführung in die Medieninformatik " "\tEMI VL\t"
-			. replace " U\tAlgorithmen und Datenstrukturen " "\tAuD Ü\t"
-			. replace " U\tMathematik 1 für Informatiker: Diskrete Strukturen & Lineare Algebra " "\tDIS+LAG Ü\t"
-			. replace " V\tMathematik 1 für Informatiker: Diskrete Strukturen & Lineare Algebra " "\tDIS+LAG VL\t"
-			. replace "\tV\tMathematik 1 für Informatiker: Diskrete Strukturen & Lineare Algebra " "\tDIS+LAG VL\t" 
-			. replace " U\tEinführung in die Medieninformatik\t" "\tEMI Ü\t"
-			. replace "\tU\tMathematik 1 für Informatiker: Diskrete Strukturen\t" "\tDIS Ü\t"
-			. replace "\tU\tEinführung in die Medieninformatik " "\tEMI Ü\t"
-			. replace " U\tTechnische Grundlagen der Informatik " "\tTGI Ü\t"
-			. replace " V\tTechnische Grundlagen der Informatik " "\tTGI VL\t"
-			. replace "\tV\tTechnische Grundlagen der Informatik " "\tTGI VL\t"
-			. replace " U\tRechnerarchitektur I " "\tRA Ü\t"
-			. replace "\tV\tRechnerarchitektur I " "\tRA VL\t"
-			. replace "\tU\tAlgorithmen und Datenstrukturen " "\tAuD Ü\t"
-			. replace " U\tMathematik 1 für Informatiker: Diskrete Strukturen & Lineare Algebra\t" "\tDIS+LAG Ü\t"
-			. replace "\n\n" "\n" . replace "\n\n" "\n" . replace "\n\n" "\n"
+    where createday els = map (\t -> if t `elem` (map sstart els) then Just (head . filter ((==) t . sstart) $ els) else Nothing) starttimes
